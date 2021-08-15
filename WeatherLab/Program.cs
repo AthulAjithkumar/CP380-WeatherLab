35f0c6ffe967355b1e40b39c9a07a6a7a8983fd6
AthulAjithkumar <74271181+AthulAjithkumar@users.noreply.github.com>
2021-08-15 11:04:28 -0400

    Update Program.cs
    updated the program.cs files

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -4,13 +4,6 @@ using System.Linq;
 
 namespace WeatherLab
 {
-    public class DegreeData
-    {
-        public int Year { get; set; }
-        public int HDD { get; set; }
-        public int CDD { get; set; }
-    }
-
     class Program
     {
         static string dbfile = @".\data\climate.db";
@@ -19,64 +12,54 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var dRF = measurements.Where(x => x.year == 2020).Sum(x => x.precipitation);
-
-            var total_2020_precipitation = dRF / measurements.Where(x => x.year == 2020).Count();
+            var total_2020_precipitation = (from data in measurements
+                                           where data.year == 2020
+                                           select data.precipitation).Sum();
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
-            //
-            // Heating Degree days have a mean temp of < 18C
-            //   see: https://en.wikipedia.org/wiki/Heating_degree_day
-            //
-
-            var data = measurements.GroupBy(x => x.year).Select(x => new
-            DegreeData()
-            {
-                Year = x.Key,
-                HDD = x.Where(x => x.meantemp < 18).Count(),
-                CDD = x.Where(x => x.meantemp >= 18).Count()
-            });
-
-            // ?? TODO ??
-
-            //
-            // Cooling degree days have a mean temp of >=18C
-            //
-
-            // ?? TODO ??
-
-            //
-            // Most Variable days are the days with the biggest temperature
-            // range. That is, the largest difference between the maximum and
-            // minimum temperature
-            //
-            // Oh: and number formatting to zero pad.
-            // 
-            // For example, if you want:
-            //      var x = 2;
-            // To display as "0002" then:
-            //      $"{x:d4}"
-            //
-
-            var vData = measurements.OrderByDescending(x => x.meantemp).Take(5).ToList();
-
+            var mean_temp = from meanYear in measurements.AsEnumerable()
+                            group meanYear by meanYear.year into resGroup
+                            orderby resGroup.Key
+                            select new { 
+                                Key = resGroup.Key,
+                                hdd = resGroup.Where(r => r.meantemp < 18).Count(),
+                                cdd = resGroup.Where(r => r.meantemp >= 18).Count(),
+                            };
+                            
             Console.WriteLine("Year\tHDD\tCDD");
-            foreach (var x in data)
-            {
-                Console.WriteLine($"\n{x.Year}\t{x.HDD}\t{x.CDD}");
+
+            foreach (var meanYear in mean_temp)
+            { 
+                Console.WriteLine($"{meanYear.Key}\t{meanYear.hdd}\t{meanYear.cdd}");
             }
 
-            
+            var variable = from temp in measurements 
+                           orderby (temp.maxtemp-temp.mintemp) descending
+                           select new
+                           {
+                               date = $"{temp.year}-{temp.month:d2}-{temp.day:d2}",
+                               delta = (temp.maxtemp - temp.mintemp),
+                              
+                           };
 
+ 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
-            foreach (var x in vData)
+
+            int count = 0;
+            foreach (var i in variable)
             {
-                Console.WriteLine($"\n{x.year}-{x.month:d2}-{x.day:d2}\t{x.meantemp}");
+                if (count < 5)
+                {
+                    Console.WriteLine($"{i.date}\t{i.delta}");
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
             }
 
-
-            // ?? TODO ??
         }
     }
 }
